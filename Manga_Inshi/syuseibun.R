ed8f108c48d28d59783ea4071d9498981fbd0a42
ma <ma_yamaki@yahoo.com>
2019-08-15 06:24:23 +0900

    momo

--- Manga_Inshi/syuseibun.R
+++ Manga_Inshi/syuseibun.R
@@ -1,1 +1,16 @@
-score_men <- c()
+score_men <- c(2, 1, 5, 2, 3, 4, 4, 1, 3, 5)
+score_gu <- c(4, 5, 3, 2, 5, 3, 4, 2, 3, 5)
+score_soup <- c(5, 1, 4, 3, 5, 2, 3, 1, 2, 3)
+tenme <- c("Niraku", "Yume", "Jima", "nanoha", "Hanabushi", "ShouTatu:", "Marukura", "Kairaku",
+           "Narumi", "Minato")
+length(tenme)
+mat_score <- cbind( score_men, score_gu, score_soup)
+row.names(mat_score) <- tenme
+
+cor(mat_score)
+momo <- prcomp(mat_score)
+plot(momo)
+eigen(mat_score)
+eigen(cor(mat_score))
+summary(momo)
+momo
