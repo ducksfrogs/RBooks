44865b27ddddb0c5042ffd99ff540c520123dcc9
ma <ma_yamaki@yahoo.com>
2019-08-17 15:32:47 +0900

    M

---
+++ rForEveryOne/edit_words.R
@@ -0,0 +1,23 @@
+person <- "Jared"
+partySize <- "eight"
+waitTime <- 25
+
+
+sprintf("Hello %s, your party of %s, will be seated in %s minuts",
+        c("Jared", "Bob"), c("egght", 16, "fore", 10), waitTime)
+require(XML)
+load("data/presidents.rdata")
+theUrl <- "http://www.loc.gov/rr/print/list/057_chron.html"
+presidents <- readHTMLTable(theUrl, which = 3, as.data.frame = TRUE,
+                            skip.rows = 1, header = TRUE,
+                            stringsAsFactors=FALSE)
+head(presidents)
+tail(presidents$YEAR)
+require(stringr)
+yearList <- str_split(string = presidents$YEAR, pattern = "-")
+yearMatrix <- data.frame(Reduce(rbind, yearList))
+names(yearMatrix) <- c("Start", "Stop")
+presidents <- cbind(presidents ,yearMatrix)
+
+head(presidents)
+tail(presidents)
