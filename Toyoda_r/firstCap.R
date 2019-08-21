7280210d1c7e69c9723df246c11daa2c4e19e7c8
ma <ma_yamaki@yahoo.com>
2019-08-21 20:01:03 +0900

    mom

---
+++ Toyoda_r/firstCap.R
@@ -0,0 +1,11 @@
+data1 <- read.csv("data/name_score.csv", header = T, row.names = 1)
+cor(data1)
+library(psych)
+myfa <- function(X, m=1,...){
+  fa(r=X, nfactors = m, fm='ml',rotate = 'promax',...)
+}
+(r_t_fa <-myfa(data1))
+
+SD_dat <- read.csv("data/Ski.csv", header =T, row.names = 1)
+SD_fa <- myfa(SD_dat, 3)
+SD_fa
