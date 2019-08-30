5af59aa6661bed745b38cb18e8c64c47238202fe
ma <ma_yamaki@yahoo.com>
2019-08-30 20:07:44 +0900

    toyoda r

---
+++ Toyoda_r/SecondChap.R
@@ -0,0 +1,16 @@
+
+library(psych)
+library(psy)
+myfa <- function(X, m=1,...){
+  fa(r=X, nfactors = m, fm='ml',rotate = 'promax',...)
+}
+SRdat <- read.csv("data/SR2.csv", header = T)
+omega.coef <- function(x, nfactors=1, fm='ml',...){
+  1 - ( sum(fa(x, nfactors = nfactors, fm=fm,...)$uniquenesses*diag(var(x)))/
+          sum(var(x)))
+}
+myfa(SRdat); cronbach(SRdat); omega.coef(SRdat)
+myscree <- function(r, b='eigen value'){
+  plot(fa(r)$e.values, type='o', xlab = b, ylab = "eigen")
+}
+myscree(SRdat)
--- Toyoda_r/firstCap.R
+++ Toyoda_r/firstCap.R
@@ -5,8 +5,25 @@ myfa <- function(X, m=1,...){
   fa(r=X, nfactors = m, fm='ml',rotate = 'promax',...)
 }
 (r_t_fa <-myfa(data1))
+r_t_fa$scores
 
 SD_dat <- read.csv("data/Ski.csv", header =T, row.names = 1)
 SD_fa <- myfa(SD_dat, 3)
 SD_fa
 
+cor(SD_dat)
+
+#factorial score
+
+SD_sc <- SD_fa$scores
+colnames(SD_sc) <- c('評価', '活動性', '力量')
+round(SD_sc,2)
+plot(SD_sc[,'評価'], SD_sc[, '活動性'], type = 'n',
+     xlab = '評価', ylab = '活動性')
+text(SD_sc[,'評価'],SD_sc[,'活動性'], rownames(SD_sc), cex = 0.8)
+
+myscree <- function(r, b='eigen value'){
+  plot(fa(r)$e.values, type='o', xlab = b, ylab = "eigen")
+}
+myscree(data1)
+myscree(SD_dat)
